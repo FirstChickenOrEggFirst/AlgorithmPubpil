31f930750a4bf7fc32f273c10fc3085a6ea2df53
wangjingbiao <577322753@qq.com>
2019-07-16 22:37:51 +0800

    增加链表实现的队列结构

---
+++ src/main/java/com/itpubpil/datastructure/queue/LinkedListQueue.java
@@ -0,0 +1,95 @@
+package com.itpubpil.datastructure.queue;
+
+/**
+ * @author wangjingbiao
+ * createTime: 2019/7/16 22:26
+ * desc:
+ * 基于链表实现队列
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void enquque(E e) {
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("cannot dequeue from an empty queue");
+        }
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return retNode.e;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("cannot get element from an empty queue");
+        }
+        return head.e;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: front ");
+
+        Node cur = head;
+        while (cur != null) {
+            res.append(cur.e + "->");
+            cur = cur.next;
+        }
+        res.append("NULL tail");
+        return res.toString();
+    }
+}
